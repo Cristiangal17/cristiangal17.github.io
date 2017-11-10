c57d114b978948b6b1e6f059e003cc746c2a98bd
Cristian Galindo <cgalindo58@gmail.com>
2017-11-10 14:26:55 -0800

    Start of something

--- javascript/WhenReady.js
+++ javascript/WhenReady.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
+    $('#contact_me_submit').on('click', function(e){
 
+        e.preventDefault();
+        var first_name = $("#contact_form_first_name").val();
+        var last_name = $("#contact_form_last_name").val();
+        var email = $('#contact_form_email').val();
+        var phone = $('#contact_form_number').val();
+        var option = $('#contact_form_input_option').val();
+        var reason = $('#ReasonForContactTextArea').val();
+        alert(first_name + " - " + last_name  + " - " + email  + " - " + phone
+            + " - " + option + " - " + reason);
+    });
 });
