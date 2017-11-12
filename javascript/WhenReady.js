83b9be828981065b08bafda1e5b4d0eff83852e5
Cristian Galindo <cgalindo58@gmail.com>
2017-11-12 00:16:27 -0800

    Organized Version of Website. The divs are organized inside each of the divs. I Still need to change the submission stuff. I need to be able to send an email still.

--- javascript/WhenReady.js
+++ javascript/WhenReady.js
@@ -1,16 +1,26 @@
-$(document).ready(function(){
-    $('#contact_me_submit').on('click', function(e){
+function sendEmail(){
 
-        e.preventDefault();
-        var first_name = $("#contact_form_first_name").val();
-        var last_name = $("#contact_form_last_name").val();
-        var email = $('#contact_form_email').val();
-        var phone = $('#contact_form_number').val();
-        var option = $('#contact_form_input_option').val();
-        var reason = $('#ReasonForContactTextArea').val();
-        alert(first_name + " - " + last_name  + " - " + email  + " - " + phone
-            + " - " + option + " - " + reason);
+    var first_name = $("#contact_form_first_name").val();
+    var last_name = $("#contact_form_last_name").val();
+    var email = $('#contact_form_email').val();
+    var phone = $('#contact_form_number').val();
+    var option = $('#contact_form_input_option').val();
+    var reason = $('#ReasonForContactTextArea').val();
+    alert(first_name + " - " + last_name  + " - " + email  + " - " + phone
+        + " - " + option + " - " + reason);
 
-
+    $.ajax({
+        url: "https://formspree.io/crs_galindo213@yahoo.com",
+        method: "POST",
+        data: {message: "hello!"},
+        dataType: "json"
     });
-});
+
+    $.ajax({
+        url: "https://formspree.io/" + email,
+        method: "POST",
+        data: {message: "hello!"},
+        dataType: "json"
+    });
+
+};
